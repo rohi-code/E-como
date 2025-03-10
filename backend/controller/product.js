ac46078dc575911e6b2c71e9c79a3e34b2bf75ef
rohi-code <rohithkonda99@gmail.com>
2025-03-10 10:19:30 +0530

    fdnbju

--- backend/controller/product.js
+++ backend/controller/product.js
@@ -194,3 +194,21 @@ if(!products)
         return res.status(500).send(e.message);
     }
 });
+
+router.get('/cartProduct',async(req,res)=>{
+    const {email} = req.query
+    try{if(!email)
+     res.status(404).send(`login to add to cart`)
+    const user = await User.findOne({email}).populate({path:'cart.productId',model:'Product'})
+    if(!user)
+        res.status(400).send(`register to add to cart`)
+    res.status(200).json({
+        message:'Cart retrived successfully',
+        cart: user.cart
+    });
+    }
+    catch(err){
+       console.error('Server error: ',err);
+       res.status(500).json({ error:'Server Error'});
+    }
+})
